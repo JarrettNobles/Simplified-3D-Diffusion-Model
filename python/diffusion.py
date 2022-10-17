cb4c63145760e14145fae49804a4c396f7e5ac41
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-10-17 17:24:47 -0400

    started python

---
+++ python/diffusion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import math
+import numpy
+
+#Jarrett Nobles
+#CSC330
+
+#main function
+def main():
+    #needs to be a constant integer
+    final int maxsize = 10;
+    #declare the array
+
+    

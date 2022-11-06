045bd303b33a2e0a617b81eeea546b75ff4b8e46
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-11-06 16:19:54 -0500

    started fortran

---
+++ fortran/diffusion.f95
@@ -0,0 +1,10 @@
+!Jarrett Nobles, CSC330
+program diffusion
+
+SUBROUTINE fill_Cube
+  !USE cube_mem
+  !partition integer here
+  !integers for the for loops for cubes
+  integer  :: i,j,k,l,m,n
+  
+  

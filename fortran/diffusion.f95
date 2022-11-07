335aff179e774d1dd4cde5329b8f277c9ccd0856
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-11-07 12:56:46 -0500

    more fortran updates

--- fortran/diffusion.f95
+++ fortran/diffusion.f95
@@ -6,5 +6,11 @@ SUBROUTINE fill_Cube
   !partition integer here
   !integers for the for loops for cubes
   integer  :: i,j,k,l,m,n
+  integer :: memStats 
+  
+  diffusion_coefficient = 0.175
+  room_dimension = 5.0
+  speed_of_gas_molecules = 250.0
+  timestep = (room_dimension / speed_of_gas_molecules) / 
   
   

cc1f5e789608f85a207eee869a41cb1191d2c8f0
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-11-08 17:13:14 -0500

    fortran updates

--- fortran/diffusion.f95
+++ fortran/diffusion.f95
@@ -2,6 +2,7 @@
 program diffusion
 
 SUBROUTINE fill_Cube
+   maxsize = 10.0
   !USE cube_mem
   !partition integer here
   !integers for the for loops for cubes
@@ -11,6 +12,15 @@ SUBROUTINE fill_Cube
   diffusion_coefficient = 0.175
   room_dimension = 5.0
   speed_of_gas_molecules = 250.0
-  timestep = (room_dimension / speed_of_gas_molecules) / 
+  timestep = (room_dimension / speed_of_gas_molecules) / maxsize 
+  DTerm = diffusion_coefficient * timestep / (distance_between_blocks*distance_between_blocks)
+  cube(0,0,0) = 1.0e21
+
+  do while(ratio <= 0.99)
+    do i = 1, maxsize
+      do j = 1, maxsize
+        do k = 1, maxsize
+          
+
   
   

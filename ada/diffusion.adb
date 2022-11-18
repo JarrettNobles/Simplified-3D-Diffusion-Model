6e470ebbc140fd13e90a08d4426cc7c02793b92f
Jarrett M. Nobles <nobles_jm@cobra.cs.mercer.edu>
2022-11-18 13:39:44 -0500

    ada almost ready

--- ada/diffusion.adb
+++ ada/diffusion.adb
@@ -5,25 +5,51 @@ use Ada.Text_IO,Ada.Float_Text_Io,Ada.Command_Line,Ada.Characters.Latin_1;
 
 procedure diffusion is
     type Three_Dimensional_Float_Array is array (Integer range <>, Integer range <>, Integer range <>) of Long_Float;
-    dim : Integer := 10;
-    room_dimension : Long_Float := 5.0;
-    diffusion_coefficent : Long_Float := 0.175;
-    speed_of_gas_molecules : Long_Float := 250.0;
-    distance_between_blocks : Long_Float := room_dimension / Long_Float(dim);
-    timestep : Long_Float := (room_dimension / speed_of_gas_molecules) / Long_Float(dim);
-    DTerm : Long_Float := diffusion_coefficent * timestep / Long_Float (distance_between_blocks*distance_between_blocks);
+    dim : Integer;
+    flag : Character;
+    tempa : Integer;
+    --tempb : Integer;
+
+    
+    
     time : Long_Float := 0.0;
     ratio : Long_Float := 0.0;
     --https://programming-idioms.org/idiom/27/create-a-3-dimensional-array/1521/ada this is where i obtained info about making this cube and array
     
     begin
+         -- ask user for input for size of the room
+         put("What is the dimension of the room: ");
+            get(dim);
+            new_line;
+            
+            --ask user for partition
+            put("Do you want to add a partition (y/n): ");
+            get(flag);
+    
         declare
+        room_dimension : Long_Float := 5.0;
+        diffusion_coefficent : Long_Float := 0.175;
+        speed_of_gas_molecules : Long_Float := 250.0;
+        distance_between_blocks : Long_Float := room_dimension / Long_Float(dim);
+        timestep : Long_Float := (room_dimension / speed_of_gas_molecules) / Long_Float(dim);
+        DTerm : Long_Float := diffusion_coefficent * timestep / Long_Float (distance_between_blocks*distance_between_blocks);
             cube : Three_Dimensional_Float_Array(1.. dim, 1.. dim, 1.. dim) := (others => (others => (others => 0.0)));
             sumval : Long_Float := 0.0;
             maxval : Long_Float := 0.0;
             minval : Long_Float := 0.0;
             change : Long_Float := 0.0;
+            tempb : Integer := Integer(Long_Float'Ceiling(Long_Float(dim) * 0.25) - 1.0);
+            
             begin
+            if flag = 'y' then
+              tempa := Integer(Long_Float'Ceiling(Long_Float(dim) * 0.5) - 1.0);
+              
+                for zerob in tempb..dim - 1 loop
+                  for zeroc in 0..dim - 1 loop
+                    cube(tempa,zerob,zeroc) := -1.0;
+                  end loop;
+                end loop;
+              end if;
                 -- initialize the first cell
                 cube(1,1,1) := 1.0e21;
                 --begin loop
@@ -64,14 +90,15 @@ procedure diffusion is
                     end loop;
                 end loop;
                 ratio := minval/maxval;
-                put(ratio);
+                put(ratio,0,9,0);
                 put("    ");
-                put(time);
+                put(time,0,5,0);
                 New_Line;
                 end loop;
             end;
             put("Box equilibrated in ");
             put(Long_Float'image(time));
             put("seconds of simulation time.");
+
 end diffusion;
 
